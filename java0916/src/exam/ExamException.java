ddb38cc63a7248635c9b5649e9d91b092299dc66
8teddy8 <8teddy8@naver.com>
2014-09-16 10:38:41 +0900

    55

---
+++ java0916/src/exam/ExamException.java
@@ -0,0 +1,37 @@
+package exam;
+
+import java.util.Scanner;
+
+/**
+ * @author KOSTA Document : ExamException Created on : 2014. 9. 16, 오전 9:46:35
+ */
+public class ExamException {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i <= 10; i++) {
+            System.out.print("수 입력 :");
+            try {
+
+                //NumberFormatException - 숫자가 아닌 값
+                int num = Integer.parseInt(sc.nextLine());
+
+                int com = (int) (Math.random() * 3);//0~2
+                // ArithmeticException - 0으로 나눌려고 할 때
+
+                int res = num / com;
+                System.out.println("출력된 결과" + res);
+            } catch (NumberFormatException e) {
+                System.out.println("숫자만 작성");
+                return;
+            } catch (ArithmeticException e) {
+                System.out.println("0으로 나눌 수 없습니다.");
+            } catch (Exception e) {
+                // 상위예외객체가 항상 아래에 와야 한다.****
+                System.out.println("숫자만 작성");
+            } finally {// 무조건 수행되는 개념!
+                System.out.println("무조건 실행되며, 자원을 반환!");
+            }
+        }
+    }
+}
